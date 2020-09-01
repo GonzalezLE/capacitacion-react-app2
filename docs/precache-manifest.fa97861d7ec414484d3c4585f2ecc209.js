0052ad1cd466b6d326ca6a4c1f2e1e3ff9712e77
GonzalezLE <gonzalez-3216@hotmail.com>
2020-09-01 17:25:40 -0500

    subiendo carpeta de produccion

---
+++ docs/precache-manifest.fa97861d7ec414484d3c4585f2ecc209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8e013a1c1049b63ce9e6245ac902ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09a4c410198f63e2d103",
+    "url": "/static/css/main.1f8a899b.chunk.css"
+  },
+  {
+    "revision": "c46504e32d6b6df90f4f",
+    "url": "/static/js/2.eaca49bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eaca49bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a4c410198f63e2d103",
+    "url": "/static/js/main.2d1db2a4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
